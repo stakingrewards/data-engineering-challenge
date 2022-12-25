a70be9b4c32803d42d0f0bb652bb1925ea5d70fa
Rafael Beckel <rafaelbeckel@Rafaels-MBP.fritz.box>
2022-12-25 02:51:58 +0100

    adds better error handling, reads file from user input and displays filtered content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{self, BufReader};
+
+use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -9,8 +12,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    println!("{} - {}", args.pattern, args.path.display());
+    let file = std::fs::File::open(&args.path)
+        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
+    let file = BufReader::new(file);
+
+    for line in io::BufRead::lines(file) {
+        let line = line.unwrap();
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
